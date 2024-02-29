2ea11467f8055bff9fceb2cf7aa5558df23db15d
金丸拓弥 <kanamarutakuya@marunaka-MacBook-Air.local>
2024-02-29 21:25:42 +0900

    Initial commit

---
+++ lib/dynamo-viewer-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class DynamoViewerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'DynamoViewerQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
